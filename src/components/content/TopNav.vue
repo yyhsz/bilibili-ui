92f5f7cfc77a8fe26ac283bc7c9ac6405517d598
yyh <591985864@qq.com>
2020-08-22 10:33:35 +0800

    添加toggleAside

--- src/components/content/TopNav.vue
+++ src/components/content/TopNav.vue
@@ -1,35 +1,40 @@
 <template>
-    <div class="top-nav">
-      <div class="logo" @click="toggleMenu">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+  <div class="top-nav">
+    <span class="toggleAside"></span>
+    <div class="logo" @click="toggleMenu">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { inject, Ref } from 'vue'
-    export default {
-        name:'TopNav',
-        setup(){
-          const showMenu = inject<Ref<boolean>>('showMenu')
-          const toggleMenu = ()=>{
-            showMenu.value = !showMenu.value
-          }
-          return{
-            toggleMenu
-          }
-        }
-    }
+import { inject, Ref } from "vue";
+export default {
+  name: "TopNav",
+  setup() {
+    const showMenu = inject<Ref<boolean>>("showMenu");
+    const toggleMenu = () => {
+      showMenu.value = !showMenu.value;
+    };
+    return {
+      toggleMenu,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .top-nav {
   background: pink;
   display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 16px;
-  position: relative;z-index: 3;
+  position: relative;
+  z-index: 3;
+
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -40,6 +45,18 @@ import { inject, Ref } from 'vue'
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+  > .toggleAside{
+    width: 24px;height: 24px;background: rgb(221, 243, 201);
+    position: absolute;left: 16px;top: 50%;transform: translateY(-50%);
+  }
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin-right: 0;
     }
   }
 }
