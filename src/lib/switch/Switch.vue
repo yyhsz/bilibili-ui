e670b7902ae23af46f721180e40d07aa8b142229
yyh <591985864@qq.com>
2020-08-23 11:21:47 +0800

    优化CSS

--- src/lib/switch/Switch.vue
+++ src/lib/switch/Switch.vue
@@ -5,19 +5,19 @@
 </template>
 
 <script>
-import {ref } from 'vue'
+import { ref } from "vue";
 export default {
-  props:{
-    checked:{
-      type:Boolean,
-      default:true
-    }
+  props: {
+    checked: {
+      type: Boolean,
+      default: true,
+    },
   },
-  setup(props,ctx) {
-    const toggle = ()=>{
-      ctx.emit('update:checked',!props.checked)
-    }
-    return {toggle};
+  setup(props, ctx) {
+    const toggle = () => {
+      ctx.emit("update:checked", !props.checked);
+    };
+    return { toggle };
   },
 };
 </script>
@@ -26,33 +26,40 @@ export default {
 $h: 22px;
 $h2: $h - 4px;
 button {
-    height: $h;
-    width: $h*2;
-    border: none;
-    border-radius: $h/2;
-    position: relative;
-    outline: none;
-    background: #bfbfbf;
-    cursor: pointer;
-    > span{
-        width: $h2;
-        height: $h2;
-        background: white;
-        border-radius: 50%;
-        position: absolute;top:2px;left: 2px;
-        transition: all 250ms ease-in-out;
-    }
-}
-button.checked{
-  background: #1890ff;
-  >span{
-    left: calc(100% - #{$h2} - 2px)
+  height: $h;
+  width: $h * 2;
+  border: none;
+  border-radius: $h/2;
+  position: relative;
+  outline: none;
+  background: #bfbfbf;
+  cursor: pointer;
+  > span {
+    width: $h2;
+    height: $h2;
+    background: white;
+    border-radius: 50%;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    transition: all 250ms ease-in-out;
   }
-}
-button:active{
-  >span{width:$h2 + 4px}
-}
-button.checked:active{
-  >span{width:$h2 + 4px;margin-left:-4px ;}
+  &.checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
 }
 </style>
