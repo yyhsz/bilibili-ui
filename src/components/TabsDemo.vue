83a9f12e926ba59396ec2c3dffc1d363f3bfac60
yyh <591985864@qq.com>
2020-09-04 08:51:41 +0800

    创建Tabs和Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,13 +1,23 @@
 <template>
-    
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script>
+import Tabs from "../lib/tabs/Tabs";
+import Tab from "../lib/tabs/Tab";
+
 export default {
   name: "Tabs",
+  components: {
+    Tabs,
+    Tab,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
