00ffdf678b836bfd424fbabc88e69d3ea92b5fb7
yyh <591985864@qq.com>
2020-09-04 22:59:55 +0800

    indicator css 1

--- src/lib/tabs/BiliTabs.vue
+++ src/lib/tabs/BiliTabs.vue
@@ -8,6 +8,7 @@
         :class="{selected:currentIndex === index}"
         @click="itemClick(index)"
       >{{item}}</div>
+      <div class="bili-tabs-nav-indicator"></div>
     </div>
     <div class="bili-tabs-content">
       <component
@@ -76,6 +77,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -86,6 +88,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
   &-content {
