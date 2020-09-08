81020b5ef8e112f25704e119a60accc5188d89c3
yyh <591985864@qq.com>
2020-09-08 20:17:53 +0800

    更改dist

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -3,6 +3,7 @@
   <section>
     <h2>使用之前</h2>
     <p>在使用bili-ui之前，我们假设您已经了解Vue、Vue组件、Vue单文件组件的基础知识。</p>
+    <p>使用bili-ui时，您需要为所有元素使用border-box盒模型，否则会影响样式。</p>
   </section>
   <section>
     <h2>搭配构建工具使用</h2>
@@ -18,16 +19,7 @@
     </pre>
     <p>以上代码便完成了bili-ui的引入。需要注意的是，样式文件需要单独引入。</p>
   </section>
-  <section>
-    <h2>特别注意</h2>
-    <p>使用bili-ui时，您需要使用border-box盒模型，否则会影响样式。CSS代码示例：</p>
-    <pre>
-      *, *::before, *::after {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }</pre>
-  </section>
+  
 </template>
 
 <script>
